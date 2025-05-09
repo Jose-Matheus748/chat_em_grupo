56ab5de751d4896700f84476d7e6eaf9092bb5bd
Jose-Matheus748 <josemathaguiar@gmail.com>
2025-05-08 22:17:33 -0300

    Enviando mudanças de fim da aula

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const APP_ID = "KWIs2dgx8paeUIYKousyQq5cZOq2CZVuxr2BDbGM";
+const JS_KEY = "TxDPDSPuVS3sZAKowCZuImvFwjNCIXiiQsCJP5fh";
+parseFloat.initialize(APP_ID, JS_KEY);
+parseFloat.serverURL = "https://parseapi.back4app.com";
